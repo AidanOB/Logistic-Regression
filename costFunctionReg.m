2805aaad26e6c758c910721287af61953792c1dc
Aidan O'Brien <aidan.obrien@student.unsw.edu.au>
2014-07-04 19:24:12 +1000

    Improved Commenting
    Improved the commenting for the cost function in the regularisation
    function.

--- costFunctionReg.m
+++ costFunctionReg.m
@@ -4,25 +4,23 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 %   theta as the parameter for regularized logistic regression and the
 %   gradient of the cost w.r.t. to the parameters. 
 
-% Initialize some useful values
-m = length(y); % number of training examples
+    % Initialize some useful values
+    m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+    % You need to return the following variables correctly 
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
+    grad = zeros(size(theta));
+    
+    %Calling the non regularised cost Function
+    [J_star, grad_star] = costFunction(theta, X, y);
 
-    [J_star grad_star] = costFunction(theta, X, y);
-
+    %Adding the regularisation to the cost
     J = J_star + lambda/(2*m)*sum(theta(2:end));
 
+    %Regularising the gradient. For j = 0
     grad(1) = grad_star(1);
-
+    
+    %Regularising the gradient for all other j
     for i = 2:length(theta)
         grad(i) = grad_star(i) + (lambda/m)* theta(i);
     end
